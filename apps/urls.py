809289f5e688fc72b8c39fea25fdae444e52ad1c
Maksim Dauhaleu <maksimdauhaleu@Maksims-MacBook-Pro.local>
2020-02-06 17:34:11 -0800

    Phone size

--- apps/urls.py
+++ apps/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.regist, name='index'),
+    url(r'^$', views.regist),
     url(r'^login$', views.login, name='login'),
     url(r'^success$', views.success, name='success'),
     url(r'^user_login$', views.user_login, name='user_login'),
